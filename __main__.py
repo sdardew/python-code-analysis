19fe77d33a7ddb4c0c35392ba6632057ab41b53d
sdardew <sdardew@gmail.com>
2021-10-12 14:13:50 +0900

    Add get_func function

--- __main__.py
+++ __main__.py
@@ -1,5 +1,4 @@
 import ast
-from collections import deque
 import re
 
 
@@ -21,29 +20,27 @@ def is_recursive(ast_tree):
     return False
 
 # binary search를 사용하는지 확인
-
-
 # import한 라이브러리를 알 수 있음
 # 수정 => body 안에서 타고 내려갈 수 있도록
-# ast_tree.dump()한 결과들만 알 수 있도록 되어 있음
-# 이거 수정해야 함
-def get_libraries(code):
-    code = code.split()
+def get_libraries(ast_tree):
+    dump_ast_tree = ast.dump(ast_tree, indent=0).split()
+    # code = code.split()
     libraries = []
     import_pattern = re.compile('name=[a-z\']+')
     import_from_pattern = re.compile('module=[a-z\']+')
 
-    for i in range(len(code)):
-        if code[i][:-1] == 'Import':
-            libraries.append(import_pattern.search(code[i+2]).group()[6:-1])
-        elif code[i][:-1] == 'ImportFrom':
-            libraries.append(import_pattern.search(code[i+3]).group()[6:-1] + ' from ' + import_from_pattern.search(code[i+1]).group()[8:-1])
+    for i in range(len(dump_ast_tree)):
+        if dump_ast_tree[i][:-1] == 'Import':
+            libraries.append(import_pattern.search(dump_ast_tree[i+2]).group()[6:-1])
+        elif dump_ast_tree[i][:-1] == 'ImportFrom':
+            libraries.append(import_pattern.search(dump_ast_tree[i+3]).group()[6:-1] + ' from ' + import_from_pattern.search(dump_ast_tree[i+1]).group()[8:-1])
 
     return libraries
 
 # list, dictionary, set, deque
 # queue 1. Collection 모듈의 deque, 2. queue 모듈의 Queue
 # flattening한 ast_tree필요 -> ast.dump(ast_tree)
+# return : dictionary로 변환 -> json형태를 위해
 def get_data_structure(ast_tree):
     return_list = []
 
@@ -78,13 +75,48 @@ def get_data_structure(ast_tree):
 
 # binary search를 사용하고 있는지 검사
 # ast.dump(ast_tree)
-def is_bisect(dump_ast_tree):
+def is_bisect(ast_tree):
+    dump_ast_tree = ast.dump(ast_tree)
     if 'Call(func=Name(id=\'bisect_left\'' in dump_ast_tree or 'Call(func=Name(id=\'bisect_right\'' in dump_ast_tree:
         return True
     return False
 
+def get_func(ast_tree):
+    # print(ast.dump(ast_tree))
+    def_pattern = re.compile(r'FunctionDef\(name=\'[A-Za-z0-9\_]+\'')
+    find_defs = def_pattern.findall(ast.dump(ast_tree))
+    defs = []
+    
+    for f in find_defs:
+        defs.append(f.split('\'')[1])
+
+    call_pattern1 = re.compile(r'Call\(func=Name\(id=\'[A-Za-z0-9\_]+\'')
+    call_pattern2 = re.compile(r'Call\(func=Attribute\(value=Name\(id=\'[A-Za-z0-9\_]+\'\, ctx=Load\(\)\)\, attr=\'[A-Za-z0-9\_]+\'')
+
+    
+    find_calls = call_pattern1.findall(ast.dump(ast_tree))
+    calls=[]
+    for f in find_calls:
+        cname = f.split('\'')[1]
+        if cname not in defs:
+            calls.append(cname)
+
+    find_calls = call_pattern2.findall(ast.dump(ast_tree))
+    for f in find_calls:
+        cname = f.split('\'')[-2]
+        if cname not in defs:
+            calls.append(cname)
+
+    print(calls)
+    return calls
+    
+
 def main(code):
     ast_tree = ast.parse(code)
+    # print(ast.dump(ast_tree, indent=4))
+
+    print("<library>")
+    print(get_libraries(ast_tree))
     
     print("<recursion>")
     print(is_recursive(ast_tree))
@@ -93,11 +125,16 @@ def main(code):
     print(get_data_structure(ast_tree))
 
     print("<binary search>")
-    print(is_bisect(ast.dump(ast_tree)))
+    print(is_bisect(ast_tree))
+
+    print('<functions>')
+    get_func(ast_tree)
 
 # binary search
 # 중첩 for문
 if __name__ == "__main__":
-
-    f = read_file('./test/test_recursion.py') # read code
-    main(f)
+    f = open("test/test_function.py", 'r')
+    try:
+        main(f.read())
+    finally:
+        f.close()
