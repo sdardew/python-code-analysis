85c580d5f6834babac559c53f7e13456456f8c99
sdardew <sdardew@gmail.com>
2021-10-12 14:14:21 +0900

    Add test file

--- test/test_function.py
+++ test/test_function.py
@@ -3,9 +3,13 @@ def recursion(num):
         return 0
     return recursion(num - 1)
 
+def return_one():
+    return 1
+
 a = [1, 10, 5, 7, 6]
 a.sort()
 a.reverse()
 a = sorted(a)
+return_one()
 
 print(recursion(3))
